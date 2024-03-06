fef4768547160882a3724e3ff8ac99d22b97b4ce
sutgeorge <georgesut@yahoo.com>
2024-03-06 09:05:43 +0200

    Fix train_data_provider error
    Now the transformers module is complaining about a missing "id2label" attribute

--- transformer_classifier.py
+++ transformer_classifier.py
@@ -6,6 +6,8 @@ import numpy as np
 import torch
 from torch.utils.data import Dataset
 
+from joblib import dump, load
+
 from transformers import AutoTokenizer
 from transformers import AutoModelForSequenceClassification
 from transformers import AutoConfig
@@ -17,6 +19,10 @@ from sklearn.metrics import precision_recall_fscore_support, accuracy_score
 from rasa.nlu.classifiers.classifier import IntentClassifier
 from rasa.engine.recipes.default_recipe import DefaultV1Recipe
 from rasa.engine.graph import ExecutionContext, GraphComponent
+from rasa.engine.storage.resource import Resource
+from rasa.shared.nlu.training_data.training_data import TrainingData
+from rasa.shared.nlu.training_data.message import Message
+from rasa.engine.storage.storage import ModelStorage
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -76,10 +82,29 @@ class TransformerClassifier(IntentClassifier, GraphComponent):
         """
         return None
 
+    @classmethod
+    def create(
+        cls,
+        config: Dict[Text, Any],
+        model_storage: ModelStorage,
+        resource: Resource,
+        execution_context: ExecutionContext,
+    ) -> GraphComponent:
+        return cls(config, execution_context.node_name, model_storage, resource)
 
-    def __init__(self, component_config=None):
-        self.model_name = component_config.get("model_name", "albert-base-v2")
-        super().__init__(component_config)
+    def __init__(
+        self,
+        config: Dict[Text, Any],
+        name: Text,
+        model_storage: ModelStorage,
+        resource: Resource,
+    ) -> None:
+        self.name = name
+        self._define_model() 
+        
+        # We need to use these later when saving the trained component.
+        self._model_storage = model_storage
+        self._resource = resource
 
     def _define_model(self):
         """
@@ -131,14 +156,14 @@ class TransformerClassifier(IntentClassifier, GraphComponent):
 
         return dataset
 
-    def train(self, train_data, cfg, **kwargs):
+    def train(self, training_data: TrainingData) -> TrainingData:
         """
         Preprocesses the data, loads the model, configures the training and trains the model.
         """
 
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
-        dataset = self._preprocess_data(train_data, self.component_config)
-        self._define_model()
+        dataset = self._preprocess_data(training_data, self.component_config)
+        # self._define_model()
 
         training_args = TrainingArguments(
             output_dir="./custom_model",
@@ -160,6 +185,10 @@ class TransformerClassifier(IntentClassifier, GraphComponent):
         )
 
         trainer.train()
+
+        self.persist()
+
+        return self._resource
 
     def _process_intent_ranking(self, outputs):
         """
@@ -203,6 +232,7 @@ class TransformerClassifier(IntentClassifier, GraphComponent):
             truncation=True,
             return_tensors="pt",
         ).to(DEVICE)
+        
         outputs = self.model(**inputs)
 
         confidence = float(outputs["logits"][0].max())
@@ -211,7 +241,7 @@ class TransformerClassifier(IntentClassifier, GraphComponent):
 
         return prediction, confidence, intent_ranking
 
-    def process(self, message, **kwargs):
+    def process(self, messages: List[Message]) -> List[Message]:
         """
         Processes the input given from Rasa. Attaches the output to the message object.
 
@@ -219,67 +249,88 @@ class TransformerClassifier(IntentClassifier, GraphComponent):
             message (Message): input message
         """
 
-        text = message.data["text"]
-        prediction, confidence, intent_ranking = self._predict(text)
+        for message in messages:
+            text = message.data["text"]
+            prediction, confidence, intent_ranking = self._predict(text)
 
-        message.set(
-            "intent", {"name": prediction, "confidence": confidence}, add_to_output=True
-        )
-        message.set("intent_ranking", intent_ranking, add_to_output=True)
+            message.set(
+                "intent", {"name": prediction, "confidence": confidence}, add_to_output=True
+            )
+            message.set("intent_ranking", intent_ranking, add_to_output=True)
+        
+        return messages
+        
+    def process_training_data(self, training_data):
+        self.process(training_data.training_examples)
+        return training_data
 
-    def persist(self, file_name, model_dir):
-        """
-        Persists the model, the tokenizer and it's configuration to the given path. Will be archived in a .tar.gz by Rasa.
+    def persist(self) -> None:
+        with self._model_storage.write_to(self._resource) as model_dir:
+            tokenizer_filename = "tokenizer_{}".format(self.name)
+            model_filename = "model_{}".format(self.name)
+            config_filename = "config_{}".format(self.name)
+            tokenizer_path = os.path.join(model_dir, tokenizer_filename)
+            model_path = os.path.join(model_dir, model_filename)
+            config_path = os.path.join(model_dir, config_filename)
+            self.tokenizer.save_pretrained(tokenizer_path)
+            self.model.save_pretrained(model_path)
+            self.config.save_pretrained(config_path)
 
-        Args:
-            file_name (str): name of the component given by Rasa based on order in the interpreter pipeline
-            model_dir (str): path to the interpreter model directory
+    # @classmethod
+    # def load(
+    #     cls, meta, model_dir=None, model_metadata=None, cached_component=None, **kwargs
+    # ):
+    #     """
+    #     Loads the model, tokenizer and configuration from the given path.
 
-        Returns:
-            model_metadata (dict): dictionary with the model, the tokenizer and the configuration names that will help load it
-        """
+    #     Returns:
+    #         component (Component): loaded component
+    #     """
 
-        tokenizer_filename = "tokenizer_{}".format(file_name)
-        model_filename = "model_{}".format(file_name)
-        config_filename = "config_{}".format(file_name)
-        tokenizer_path = os.path.join(model_dir, tokenizer_filename)
-        model_path = os.path.join(model_dir, model_filename)
-        config_path = os.path.join(model_dir, config_filename)
-        self.tokenizer.save_pretrained(tokenizer_path)
-        self.model.save_pretrained(model_path)
-        self.config.save_pretrained(config_path)
+    #     tokenizer_filename = meta.get("tokenizer")
+    #     model_filename = meta.get("model")
+    #     config_filename = meta.get("config")
+    #     tokenizer_path = os.path.join(model_dir, tokenizer_filename)
+    #     model_path = os.path.join(model_dir, model_filename)
+    #     config_path = os.path.join(model_dir, config_filename)
 
-        return {
-            "config": config_filename,
-            "tokenizer": tokenizer_filename,
-            "model": model_filename,
-        }
+    #     x = cls(meta)
+    #     x.tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
+    #     x.config = AutoConfig.from_pretrained(config_path)
+    #     x.id2label = x.config.id2label
+    #     x.label2id = x.config.label2id
+    #     x.model = AutoModelForSequenceClassification.from_pretrained(
+    #         model_path, config=x.config
+    #     ).to(DEVICE)
+
+    #     return x
 
     @classmethod
     def load(
-        cls, meta, model_dir=None, model_metadata=None, cached_component=None, **kwargs
-    ):
-        """
-        Loads the model, tokenizer and configuration from the given path.
+        cls,
+        config: Dict[Text, Any],
+        model_storage: ModelStorage,
+        resource: Resource,
+        execution_context: ExecutionContext,
+    ) -> GraphComponent:
+        with model_storage.read_from(resource) as model_dir:
+            component = cls(
+                config, execution_context.node_name, model_storage, resource
+            )
 
-        Returns:
-            component (Component): loaded component
-        """
+            tokenizer_filename = config["tokenizer"]
+            model_filename = config["model"]
+            config_filename = config["config"]
+            tokenizer_path = os.path.join(model_dir, tokenizer_filename)
+            model_path = os.path.join(model_dir, model_filename)
+            config_path = os.path.join(model_dir, config_filename)
 
-        tokenizer_filename = meta.get("tokenizer")
-        model_filename = meta.get("model")
-        config_filename = meta.get("config")
-        tokenizer_path = os.path.join(model_dir, tokenizer_filename)
-        model_path = os.path.join(model_dir, model_filename)
-        config_path = os.path.join(model_dir, config_filename)
+            component.tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
+            component.config = AutoConfig.from_pretrained(config_path)
+            component.id2label = component.config.id2label
+            component.label2id = component.config.label2id
+            component.model = AutoModelForSequenceClassification.from_pretrained(
+                model_path, config=component.config
+            ).to(DEVICE)
 
-        x = cls(meta)
-        x.tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
-        x.config = AutoConfig.from_pretrained(config_path)
-        x.id2label = x.config.id2label
-        x.label2id = x.config.label2id
-        x.model = AutoModelForSequenceClassification.from_pretrained(
-            model_path, config=x.config
-        ).to(DEVICE)
-
-        return x
+            return component
